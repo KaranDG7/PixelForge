8c60db152e0a213b8e892a644cbef98c255db2c1
KaranDG7 <karangonbare@gmail.com>
2024-09-22 15:24:47 +0530

    Initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '@/components/ui/shared/sidebar'
+import MobileNav from '@/components/ui/shared/MobileNav'
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+        <div className="root-container">
+            <div className="wrapper">
+                {children}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Layout
