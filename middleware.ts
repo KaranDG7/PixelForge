f3bf3aca2795544af508b4ed12c5f9063c31b4e7
KaranDG7 <karangonbare@gmail.com>
2024-09-28 12:14:20 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,24 @@
-// middleware.ts or middleware.js
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// // middleware.ts or middleware.js
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 
+// export const config = {
+//   matcher: [
+//     // Apply middleware to all API routes except '/api/webhooks/clerk'
+//     '/api((?!/webhooks/clerk).*)',
+//     // Match all tRPC routes
+//     '/trpc/:path*',
+//     // Skip Next.js internals and all static files
+//     '/((?!_next|[^?]*\\.(?:html?|css|js|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//   ],
+// };
+import { authMiddleware } from "@clerk/nextjs/dist/types/server";
+ 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
+ 
 export const config = {
-  matcher: [
-    // Apply middleware to all API routes except '/api/webhooks/clerk'
-    '/api((?!/webhooks/clerk).*)',
-    // Match all tRPC routes
-    '/trpc/:path*',
-    // Skip Next.js internals and all static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
